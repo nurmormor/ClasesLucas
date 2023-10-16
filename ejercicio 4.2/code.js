0741aa05933c4ba361b0b3f821ae7bee0da19702
Nuria Moreno <nurmormor@gmail.com>
2023-10-16 22:05:41 +0200

    nuevo

--- ejercicio 4.2/code.js
+++ ejercicio 4.2/code.js
@@ -8,7 +8,7 @@ divResultado.innerHTML += "<p> el doble del número es: " + numero*2 + "</p>" + 
 
 divResultado.innerHTML += "<p> el triple del número es: " + numero*3 + "</p>" + "<br>";
 
-divResultado.innerHTML += "<p> el cuadrado del número es: " + numero+numero + "</p>" + "<br>";
+divResultado.innerHTML += "<p> el cuadrado del número es: " + numero*numero + "</p>" + "<br>";
 
 divResultado.innerHTML += "<p> la mitad del número es: " + numero/2 + "</p>" + "<br>";
 
